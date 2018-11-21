744af3d770cb92616e7ea70d44853d43fc8f0290
Alexey Rekish <alexeyrekish@gmail.com>
2018-11-21 13:23:53 +0300

    layout fixes

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ gulp.task('images', function() {
 gulp.task('webp', function() {
     return gulp
         .src('src/img/**/*.{png,jpg}')
-        .pipe(webp({ quality: 100 }))
+        .pipe(webp({ quality: 75 }))
         .pipe(gulp.dest('build/img'));
 });
 
