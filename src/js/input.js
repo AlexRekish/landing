bea08e907b49c5d6e0240cde02e8fbb276db492e
Alexey Rekish <alexeyrekish@gmail.com>
2018-11-21 18:37:14 +0300

    added carousel

--- src/js/input.js
+++ src/js/input.js
@@ -1,2 +1,69 @@
-let a = 5;
-console.log(a);
+'use strict';
+
+// localization
+
+const l10n = document.querySelector('.page-header__l10n');
+const l10nList = document.querySelector('.page-header__l10n-list');
+const l10nActive = document.querySelector('.page-header__l10n-active');
+
+const openL10n = evt => {
+    evt.preventDefault();
+    l10nList.classList.toggle('page-header__l10n-list--open');
+};
+
+l10n.addEventListener('click', openL10n);
+
+// reviews-carousel
+
+const nextReviewButtonDesk = document.querySelector('.android-reviews__next-review--desk');
+const nextReviewButtonMob = document.querySelector('.android-reviews__next-review--mob');
+const carousel = document.querySelector('.android-reviews__reviews-carousel');
+const reviews = document.querySelectorAll('.android-reviews__review');
+const reviewers = document.querySelectorAll('.android-reviews__reviewer-ava--desk');
+
+let translateDesk = 0;
+let translateMob = 0;
+let paddings = 0;
+
+const nextReviewDesk = evt => {
+    evt.preventDefault();
+    translateDesk -= 314;
+    translateDesk = Math.abs(translateDesk) < 1570 ? translateDesk : 0;
+    carousel.style.transform = `translateX(${translateDesk}px)`;
+
+    for (let i = 0; i < reviewers.length; i++) {
+        if (reviewers[i].classList.contains('android-reviews__reviewer-ava--active')) {
+            if (i + 1 < reviewers.length) {
+                reviewers[i].classList.remove('android-reviews__reviewer-ava--active');
+                reviewers[i + 1].classList.add('android-reviews__reviewer-ava--active');
+                break;
+            }
+            reviewers[i].classList.remove('android-reviews__reviewer-ava--active');
+            reviewers[0].classList.add('android-reviews__reviewer-ava--active');
+        }
+    }
+};
+const nextReviewMob = evt => {
+    evt.preventDefault();
+    translateMob -= 100;
+    paddings += 32;
+
+    for (let i = 0; i < reviews.length; i++) {
+        if (reviews[i].classList.contains('android-reviews__review--active')) {
+            if (i + 1 < reviews.length) {
+                carousel.style.transform = `translateX(calc(${translateMob}vw + ${paddings}px))`;
+                reviews[i].classList.remove('android-reviews__review--active');
+                reviews[i + 1].classList.add('android-reviews__review--active');
+                break;
+            }
+            translateMob = 0;
+            paddings = 0;
+            carousel.style.transform = `translateX(${translateMob}vw)`;
+            reviews[i].classList.remove('android-reviews__review--active');
+            reviews[0].classList.add('android-reviews__review--active');
+        }
+    }
+};
+
+nextReviewButtonDesk.addEventListener('click', nextReviewDesk);
+nextReviewButtonMob.addEventListener('click', nextReviewMob);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -70,8 +70,73 @@
 "use strict";
 
 
-var a = 5;
-console.log(a);
+// localization
+
+var l10n = document.querySelector('.page-header__l10n');
+var l10nList = document.querySelector('.page-header__l10n-list');
+var l10nActive = document.querySelector('.page-header__l10n-active');
+
+var openL10n = function openL10n(evt) {
+    evt.preventDefault();
+    l10nList.classList.toggle('page-header__l10n-list--open');
+};
+
+l10n.addEventListener('click', openL10n);
+
+// reviews-carousel
+
+var nextReviewButtonDesk = document.querySelector('.android-reviews__next-review--desk');
+var nextReviewButtonMob = document.querySelector('.android-reviews__next-review--mob');
+var carousel = document.querySelector('.android-reviews__reviews-carousel');
+var reviews = document.querySelectorAll('.android-reviews__review');
+var reviewers = document.querySelectorAll('.android-reviews__reviewer-ava--desk');
+
+var translateDesk = 0;
+var translateMob = 0;
+var paddings = 0;
+
+var nextReviewDesk = function nextReviewDesk(evt) {
+    evt.preventDefault();
+    translateDesk -= 314;
+    translateDesk = Math.abs(translateDesk) < 1570 ? translateDesk : 0;
+    carousel.style.transform = 'translateX(' + translateDesk + 'px)';
+
+    for (var i = 0; i < reviewers.length; i++) {
+        if (reviewers[i].classList.contains('android-reviews__reviewer-ava--active')) {
+            if (i + 1 < reviewers.length) {
+                reviewers[i].classList.remove('android-reviews__reviewer-ava--active');
+                reviewers[i + 1].classList.add('android-reviews__reviewer-ava--active');
+                break;
+            }
+            reviewers[i].classList.remove('android-reviews__reviewer-ava--active');
+            reviewers[0].classList.add('android-reviews__reviewer-ava--active');
+        }
+    }
+};
+var nextReviewMob = function nextReviewMob(evt) {
+    evt.preventDefault();
+    translateMob -= 100;
+    paddings += 32;
+
+    for (var i = 0; i < reviews.length; i++) {
+        if (reviews[i].classList.contains('android-reviews__review--active')) {
+            if (i + 1 < reviews.length) {
+                carousel.style.transform = 'translateX(calc(' + translateMob + 'vw + ' + paddings + 'px))';
+                reviews[i].classList.remove('android-reviews__review--active');
+                reviews[i + 1].classList.add('android-reviews__review--active');
+                break;
+            }
+            translateMob = 0;
+            paddings = 0;
+            carousel.style.transform = 'translateX(' + translateMob + 'vw)';
+            reviews[i].classList.remove('android-reviews__review--active');
+            reviews[0].classList.add('android-reviews__review--active');
+        }
+    }
+};
+
+nextReviewButtonDesk.addEventListener('click', nextReviewDesk);
+nextReviewButtonMob.addEventListener('click', nextReviewMob);
 
 /***/ })
 /******/ ]);
